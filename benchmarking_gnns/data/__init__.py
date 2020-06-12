677d34811f9805a7f2af770c5e2f85cb9a10b018
Jacob R. Stevens <steven69@purdue.edu>
2020-06-12 07:28:17 -0500

    reorg

---
+++ benchmarking_gnns/data/__init__.py
@@ -0,0 +1,6 @@
+from .superpixels import SuperPixDataset
+from .molecules import MoleculeDataset
+from .TUs import *
+from .SBMs import SBMsDataset
+from .TSP import TSPDataset
+from .CitationGraphs import CitationGraphsDataset
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+     name='benchmarking_gnns',  
+     version='0.1',
+     author="Jacob R. Stevens",
+     author_email="steven69@purdue.edu",
+     description="Benchmarking GNNs",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+
+ )
